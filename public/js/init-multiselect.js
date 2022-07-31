d4f17ab692ffade4321e7d4210e7cb5b92c9f5ad
FadyKhalaf <fadykhalaf01@gmail.com>
2022-07-31 12:42:22 -0700

    fill multi-select UI element with approperiate values

--- public/js/init-multiselect.js
+++ public/js/init-multiselect.js
@@ -1,14 +1,44 @@
 $(function() {
-    let myOptions = ['one', 'two', 'three'];
-    console.log('here');
-    VirtualSelect.init({
-        ele: '#example-select',
-        options: myOptions,
-        multiple: true,
-        search: true,
-        searchGroup: false, // Include group title for searching
-        searchByStartsWith: true, // Search options by startsWith() method
-        placeholder: 'اختار 3 اسماء بالكتير'
-    });
 
+    const initMultiSelect = (id, collection) => {
+        VirtualSelect.init({
+            ele: id,
+            options: collection,
+            multiple: true,
+            search: true,
+            searchGroup: false, // Include group title for searching
+            searchByStartsWith: true, // Search options by startsWith() method
+            placeholder: 'اختار 3 اسماء بالكتير'
+        });
+    };
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/users",
+        contentType: 'application/json',
+        success: function(response) {
+            let boys = response.boys.map(boy => boy.name);
+            let girls = response.girls.map(girl => girl.name)
+            initMultiSelect('#boys-select', boys);
+            initMultiSelect('#girls-select', girls);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Failed to get boys and girls not in a room!!!");
+        },
+    });    
+
+    /* add event lister to the select for boys and girls */
+    const roomTypeSelectElement = document.querySelector('#room-type');
+    const boysMultiSelect = document.querySelector('#boys-select');
+    const girlsMultiSelect = document.querySelector('#girls-select');
+
+    roomTypeSelectElement.addEventListener('change', (event) => {
+        girlsMultiSelect.classList.remove('hidden');
+        boysMultiSelect.classList.remove('hidden');
+        if (event.target.value === 'boys') {
+            girlsMultiSelect.classList.add('hidden');
+        } else {
+            boysMultiSelect.classList.add('hidden');
+        }
+      });
 });
