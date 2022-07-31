6e3a6e27f47cd21a61936d19cc25bace08d3fac0
FadyKhalaf <fadykhalaf01@gmail.com>
2022-07-31 15:44:49 -0700

    basic: get basic form input when submitting

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,23 @@ $(function() {
 	  $(this).closest('.accordion-item').removeClass('active');
 	});
 
+	$('form').submit((event) => {
+		event.preventDefault();
+		const roomType = $('#room-type').find(":selected").val();
+		let selectedNames = null;
+		if (roomType === 'boys') {
+			selectedNames = $('#boys-select').val();
+		} else {
+			selectedNames = $('#girls-select').val();
+		}
+		
+		if (selectedNames.length < 2) {
+			alert('من فضلك اختار على الاقل اسمين');
+		} else {
+			console.log(roomType);
+			console.log(selectedNames);
+		}
+	})
     
 
 });
