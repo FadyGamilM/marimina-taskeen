30f0d678f0c9ed077f3d379c9323e1faf2a898fb
FadyKhalaf <fadykhalaf01@gmail.com>
2022-08-09 10:31:23 -0700

    adding more instructions - limiting room number to 3 instead of 4

--- public/js/main.js
+++ public/js/main.js
@@ -22,25 +22,25 @@ $(function() {
 			selectedNames = $('#families-select').val();
 		}
 		
-		if (roomType !== 'families' && (selectedNames.length < 1 || selectedNames.length > 3)) {
-			alert('من فضلك اختار من اسم حتى ثلاثة اسماء لغرف الاولاد و البنات');
-		} else if (roomType === 'families' && (selectedNames.length < 1 || selectedNames.length > 4)) {
-			alert('من فضلك اختار من اسم حتى 4 اسماء للغرف العائلية');
+		if (selectedNames.length < 1 || selectedNames.length > 3) {
+			alert('من فضلك اختار من اسم حتى ثلاثة اسماء');
 		} else {
-			$('#submit').prop('disabled', true);
-			$.ajax({
-				type: "POST",
-				url: 'https://marimina-tasken.herokuapp.com/rooms', //TODO....fix this by adding support for CORS
-				contentType: 'application/json',
-				data: JSON.stringify({roomType, selectedNames, notes}),
-				success: function(response) {
-					alert("تم انشاء غرفة بنجاح");
-					window.location.reload(true);
-				},
-				error: function(jqXHR, textStatus, errorThrown) {
-					alert("Failed to get boys and girls not in a room!!!");
-				},
-			});   
+			if (confirm(`متأكد انك عايز تسكن ${selectedNames} مع بعض؟`)) {
+				$('#submit').prop('disabled', true);
+				$.ajax({
+					type: "POST",
+					url: 'https://marimina-tasken.herokuapp.com/rooms', //TODO....fix this by adding support for CORS
+					contentType: 'application/json',
+					data: JSON.stringify({roomType, selectedNames, notes}),
+					success: function(response) {
+						alert("تم انشاء غرفة بنجاح");
+						window.location.reload(true);
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						alert("Failed to get boys and girls not in a room!!!");
+					},
+				});   
+			}
 		}
 	})
     
